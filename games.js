d308b1beaf7ff1e6a58fc43b37b517858f28c040
Andy Olsen <andy.olsen.tx@gmail.com>
2015-01-24 12:29:43 -0600

    Remove task list example code

--- games.js
+++ games.js
@@ -4,33 +4,17 @@
 //   games
 //     game_roles
 
-Tasks    = new Mongo.Collection("tasks");
 AllRoles = new Mongo.Collection("all_roles");
 Games    = new Mongo.Collection("games");
 GamePlayers = new Mongo.Collection("game_players");
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Meteor.subscribe("tasks");
   Meteor.subscribe("all_roles");
   Meteor.subscribe("games");
   Meteor.subscribe("game_players");
   Meteor.subscribe("directory");
   Template.body.helpers({
-    tasks: function () {
-      if (Session.get("hideCompleted")) {
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        // Show newest tasks first
-        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
-    },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-    },
-    incompleteCount: function() {
-      return Tasks.find({checked: {$ne: true}}).count();
-    },
     users: function() {
       return Meteor.users.find({});
     },
@@ -48,11 +32,6 @@ if (Meteor.isClient) {
       return result;
     },
   });
-  Template.task.helpers({
-    isOwner: function() {
-      return this.owner == Meteor.userId();
-    }
-  });
 
   function isPlayingThisGameTest(game) {
     var gamePlayerId = GamePlayers.findOne({gameid:game._id, userid:Meteor.userId()});
@@ -61,16 +40,6 @@ if (Meteor.isClient) {
 
 
   Template.body.events({
-    "submit .new-task": function (event) {
-      var text = event.target.text.value;
-      Meteor.call("addTask", text);
-      event.target.text.value = "";
-      // Prevent default form submit
-      return false;
-    },
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    },
     "click .add-game": function () {
       Meteor.call("addGame");
     },
@@ -87,57 +56,12 @@ if (Meteor.isClient) {
     },
   });
 
-  Template.task.events({
-    "click .toggle-checked": function () {
-      Meteor.call("setChecked", this._id, !this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
-    },
-    "click .toggle-private": function() {
-      Meteor.call("setPrivate", this._id, !this.private);
-    },
-  });
-
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
 Meteor.methods({
-  addTask: function (text) {
-    // Make sure the user is logged in before inserting a task
-    if (! Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.insert({
-      text: text,
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
-  },
-  deleteTask: function (taskId) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.remove(taskId);
-  },
-  setChecked: function (taskId, setChecked) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.update(taskId, { $set: { checked: setChecked} });
-  },
-  setPrivate: function (taskId, setToPrivate) {
-    var task = Tasks.findOne(taskId);
-    if (task.owner !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.update(taskId, { $set: {private: setToPrivate}});
-  },
   addGame: function () {
     if (!Meteor.userId()) { throw new Meteor.Error("not-authorized"); }
     var gameId = Games.insert({ createdAt: new Date(), text: "Game created by "+Meteor.user().username, owner: Meteor.userId()});
@@ -158,14 +82,6 @@ Meteor.methods({
 });
 
 if (Meteor.isServer) {
-  Meteor.publish("tasks", function() {
-    return Tasks.find({
-      $or: [
-        { private: {$ne: true}},
-        { owner  : this.userId }
-      ]
-    });
-  });
   Meteor.publish("all_roles", function() {
     return AllRoles.find({});
   });
